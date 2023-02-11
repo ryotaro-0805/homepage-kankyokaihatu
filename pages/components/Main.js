37f0ce288246e07d07e9c9ccc1ba57386af9d598
ryotaro-0805 <rasouryo@yahoo.co.jp>
2023-02-11 23:27:18 +0900

    ✅ 元に戻しました

--- pages/components/Main.js
+++ pages/components/Main.js
@@ -14,8 +14,7 @@ export default function Main() {
             </Head>
             <div className={`contentsMain ${styles.main_container}`}>
                 <Image className={styles.main_image_pic} src={main_pic} alt='top-pic' />
-                <h2 className={styles.main_h2}>明るい未来へあなたと共に<br />いいえ僕たちです</h2>
-                <h2 className={styles.main_h2}>さらにもう一言言いいたい</h2>
+                <h2 className={styles.main_h2}>明るい未来へあなたと共に</h2>
             </div>
         </>
     )
